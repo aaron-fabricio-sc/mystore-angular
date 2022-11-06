f5d27fdc34f238baaf9a2109701006f8de3937e8
aaron fabricio <aaronfabricio00@gmail.com>
2022-11-05 20:41:24 -0400

    primer guardado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './produc-model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,69 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myStore';
+  nombre = "aaron";
+
+  btnvalor = true
+
+  ngName = ""
+
+  palabras : string[]= ["ala","te" , "jodes"] 
+  palabra :string = ""
+
+
+  productos: Product[] = [
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+    {
+      name:"aleatorio",
+      price:1111,
+      img :"https://api.lorem.space/image/movie?w=150&h=220"
+    },
+  ] 
+
+
+ 
+
+  chanlleName(event : Event){
+    const element = event.target as HTMLInputElement;
+
+    this.nombre = element.value
+  }
+
+  newPalabra(){
+    this.palabras.push(this.palabra)
+    this.palabra =""
+  }
+
+  deletePalabra(index : number){
+    this.palabras.splice(index,1)
+  }
 }
